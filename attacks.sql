45e4e2bcf26988aa59cf067f84ecbd474dc69888
Luca Demetrio <luca.demetrio@outlook.it>
2020-06-01 14:46:46 +0200

    Removing lfs

--- attacks.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5cb657d2603d5dcc74b3acac6f57a1875ccee52e79ef5ca80bf985607227922c
-size 171481200
--- sane.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:431ac9c65c85794f4ba25904b46f2971d41b434697d9c30760c5a1fe05ac47ec
-size 75234679
